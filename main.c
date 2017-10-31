bcd77fd8a296b81f7e13ab5375d496be8157a8e7
sabrina-wen <sabrinawn253@gmail.com>
2017-10-31 19:59:38 -0400

    added first part

--- main.c
+++ main.c
@@ -1,3 +1,16 @@
-int main() {
+#include <sys/stat.h>
+#include <stdio.h>
+#include <time.h>
 
+void get_info(char * file_name) {
+  struct stat file;
+  if (stat(file_name, &file) == 0) {
+    printf("file size: %lu\n", file.st_size);
+    printf("mode: %d\n", file.st_mode);
+    printf("time last accessed: %s\n", ctime(&file.st_atime));
+  }
+}
+
+int main() {
+  get_info("main.c");
 }
