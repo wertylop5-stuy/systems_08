a1a933ea610204db000f47648bc22930adbfe8d9
Stanley Lin <slin55055@gmail.com>
2017-10-31 15:45:49 -0400

    work

--- main.c
+++ main.c
@@ -1,3 +1,42 @@
-int main() {
+#include<stdio.h>
+#include<sys/stat.h>
 
+void better_size(off_t size) {
+	float more_info = (float)size;
+	
+	if (size < 1024) {
+		printf("B: %lu\n", size);
+		return;
+	}
+	
+	char counter = 0;
+	while(more_info > 1024) {
+		counter++;
+		more_info /= 1024;
+	}
+	
+	printf("counter: %d\n", counter);
+	switch(counter) {
+		case 0:
+			printf("B: %f\n", more_info);
+		break;
+		
+		case 1:
+			printf("KB: %f\n", more_info);
+		break;
+
+		case 2:
+			printf("MB: %f\n", more_info);
+		break;
+	
+		case 3:
+			printf("GB: %f\n", more_info);
+		break;
+	}
+}
+
+int main() {
+	struct stat sb;
+	stat("a.out", &sb);
+	better_size(sb.st_size);
 }
