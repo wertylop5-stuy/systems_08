425fb5a8599283a97e5ee7c2c1bdfd3f9a5e9136
sabrina-wen <sabrinawn253@gmail.com>
2017-10-31 20:34:37 -0400

    added errno

--- main.c
+++ main.c
@@ -1,8 +1,7 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<sys/stat.h>
+#include <sys/stat.h>
 #include <stdio.h>
 #include <time.h>
+#include <errno.h>
 
 void get_info(char * file_name) {
   struct stat file;
@@ -10,16 +9,17 @@ void get_info(char * file_name) {
     printf("file size: %lu\n", file.st_size);
     printf("mode: %d\n", file.st_mode);
     printf("time last accessed: %s\n", ctime(&file.st_atime));
+  } else { //!
+    printf("%d: %s\n", errno, strerror(errno)); 
   }
 }
 
-char* better_size(off_t size) {
+void better_size(off_t size) {
 	float more_info = (float)size;
-	char *res = malloc(50*sizeof(char));
 	
 	if (size < 1024) {
-		sprintf(res, "B: %lu", size);
-		return res;
+		printf("B: %lu\n", size);
+		return;
 	}
 	
 	char counter = 0;
@@ -28,33 +28,31 @@ char* better_size(off_t size) {
 		more_info /= 1024;
 	}
 	
+	printf("counter: %d\n", counter);
 	switch(counter) {
 		case 0:
-			sprintf(res, "B: %f", more_info);
+			printf("B: %f\n", more_info);
 		break;
 		
 		case 1:
-			sprintf(res, "KB: %f", more_info);
+			printf("KB: %f\n", more_info);
 		break;
 
 		case 2:
-			sprintf(res, "MB: %f", more_info);
+			printf("MB: %f\n", more_info);
 		break;
 	
 		case 3:
-			sprintf(res, "GB: %f", more_info);
+			printf("GB: %f\n", more_info);
 		break;
 	}
-	return res;
 }
 
 int main() {
+        get_info("main.c");
 	struct stat sb;
-	stat("main.c", &sb);
-	printf("%s\n", better_size(sb.st_size));
-	
 	stat("a.out", &sb);
-	printf("%s\n", better_size(sb.st_size));
+	better_size(sb.st_size);
 
 	return 0;
 }
